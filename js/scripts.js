3a09f2121c820f662affb6c61b0811d2405efedf
stephaniebippert <87578448+stephaniebippert@users.noreply.github.com>
2021-08-14 13:04:14 -0400

    Update scripts.js
    added JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
 console.log("Hello World, Welcome to My Portolio Website!")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+}
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
